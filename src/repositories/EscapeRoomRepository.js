14aaab3adcc8f2c74e72995e4355b0897210bf22
Luke <lukegothic@gmail.com>
2024-02-04 15:56:03 +0100

    update escapes

--- src/repositories/EscapeRoomRepository.js
+++ src/repositories/EscapeRoomRepository.js
@@ -1,13 +1,15 @@
 import ERLDecrypter from '../ui/functions/ERLDecrypter';
-
+import escape_rooms from '../data/escaperoomlover.company.all.json';
 const EscapeRoomRepository = {
     get: async () => {
         const endpoint = "https://www.escaperoomlover.com/api/es/public/company/all";
         const proxy = "https://corsicorsi.herokuapp.com";
         const cacheOptions = { cache: "force-cache" };
-        const response = await fetch(`${proxy}/${endpoint}`, cacheOptions);
-        const json = await response.json();
-        return JSON.parse(ERLDecrypter.decrypt(json));
+        // const response = await fetch(`${proxy}/${endpoint}`, cacheOptions);
+        //const response = await fetch("escaperoomlover.company.all.json");
+        //const json = await response.json();
+        const d = ERLDecrypter.decrypt(escape_rooms);
+        return JSON.parse(d);
     }
 }
 export default EscapeRoomRepository;
